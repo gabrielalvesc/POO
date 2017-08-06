fc9b7544ab4e455b0a745020c3f4483b6bd5ccd6
gabrielalvesc <gabriels.alvess@gmail.com>
2017-08-06 18:29:33 -0300

    Change Resposta6 to Resposta7

--- Lista I - POO/src/com/ufpb/questao1/Main.java
+++ Lista I - POO/src/com/ufpb/questao1/Main.java
@@ -14,16 +14,16 @@ public class Main {
 		System.out.println("Largura: ");
 		Double largura = scan.nextDouble();
 		r1.setLargura(largura);
-		System.out.println(r1.toString()+"\nPerÃ­metro: "+r1.calculaPerimetro(altura, largura)+"\nÃ�rea: "+r1.calculaArea(altura, largura)+"\n");
+		System.out.println(r1.toString()+"\nPerí­metro: "+r1.calculaPerimetro(altura, largura)+"\nÁrea: "+r1.calculaArea(altura, largura)+"\n");
 		Retangulo r2 = new Retangulo(2.0, 5.5);
 		Double area = r2.calculaArea(2.0, 5.5);
 		Double perimetro = r2.calculaPerimetro(2.0, 5.5);
-		System.out.println(r2.toString()+"\nPerÃ­metro: "+perimetro+"\nÃ�rea: "+area+"\n");
+		System.out.println(r2.toString()+"\nPerí­metro: "+perimetro+"\nÁrea: "+area+"\n");
 		boolean igual = r1.equals(r2);
 		if(igual == true){
-			System.out.println("SÃ£o iguais!!!");
+			System.out.println("São iguais!!!");
 		} else {
-			System.out.println("NÃ£o sÃ£o iguais!!!");
+			System.out.println("Não são iguais!!!");
 		}
 		scan.close();
 	}
---
+++ Lista I - POO/src/com/ufpb/questao7/Resposta7.java
@@ -0,0 +1,9 @@
+package com.ufpb.questao7;
+
+public class Resposta7 {
+	
+	/*	Herança é quando uma classe herdaa outra, ou seja, a ligação entre elas é muito forte.
+	 * 	Já à composição estende uma classe através de delegação de trabalho atribuida a ela.
+	 */
+
+}
