2138b3c66e9a5efa61b7e82fe72c99897c0a1e3f
gabrielalvesc <gabriels.alvess@gmail.com>
2017-08-06 20:32:52 -0300

    Add Resposta6

---
+++ Lista I - POO/src/com/ufpb/questao6/Resposta6.java
@@ -0,0 +1,15 @@
+package com.ufpb.questao6;
+
+public class Resposta6 {
+	
+	/*	A interface é como um contrato que rege um acordo entre as partes, ou seja, define um determinado 
+	 * 	conjunto de métodos que serão obrigatoriamente implementados nas classes que assinarem este contrato.
+	 * 	As classes abstratas são classes que servem como modelos a serem seguidos por classes que herdem dela,
+	 * 	e não podem ser estanciadas. Os métodos da classe abstrata devem ser sobrescritos nas classes filhas.
+	 * 	
+	 * 	Devemos usar interfaces quando diferentes classes 
+	 * 	
+	 * 
+	 */
+
+}
